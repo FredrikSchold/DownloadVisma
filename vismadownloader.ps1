c8b1187c7734e9b26888a5e679863eedb7f49358
FredrikSchold <71184994+FredrikSchold@users.noreply.github.com>
2021-01-26 15:43:35 +0100

    Update vismadownloader.ps1

--- vismadownloader.ps1
+++ vismadownloader.ps1
@@ -2,10 +2,17 @@
 #https://download.vismaspcs.se/downloads/visma_bokslut_2020.12_setup.exe
 #https://download.vismaspcs.se/downloads/visma_byrastod_6.0_setup.exe
 #https://download.vismaspcs.se/downloads/visma_koncern_2021.0_setup.exe
+##################################################################################################################################
+# Vismadownloader Version 1.0
+#
+# PS-script för att ladda ned senaste versionerna av visma. Man kan lätt utveckla detta vidare för andra vismavarianter. 
+# Beskriver mer i koden nedan
+#
+##################################################################################################################################
 
-$ver = 0
+$ver = 0 #Version
 
-
+#Function för att titta igenom vilka versioner av visma som finns att ladda ned hos dem
 function download{
     
       
@@ -28,12 +35,13 @@ function download{
 }
 }
 
-
+# Varje kodsnutt nedan specificerar vilken variant av visma som ska laddas ner, hämtar automatiskt via funktionen "download" årets senaste och flyttar till rätt mapp
 $visma = "visma_bokslut_"
 $date = Get-date -Format "yyyy"
 download
 Move-Item -path "D:\Data\gemensam\data\Software\Visma\*.exe" -Destination "D:\Data\gemensam\data\Software\Visma\Visma bokslut" -Force
 
+# Varje kodsnutt nedan specificerar vilken variant av visma som ska laddas ner, hämtar automatiskt via funktionen "download" årets senaste och flyttar till rätt mapp
 $date = 0
 $visma = "visma_byrastod_"
 while ($date -le 10) {
@@ -47,6 +55,7 @@ $date++
 }
 }
 
+# Varje kodsnutt nedan specificerar vilken variant av visma som ska laddas ner, hämtar automatiskt via funktionen "download" årets senaste och flyttar till rätt mapp
 $visma = "visma_koncern_"
 $date = Get-date -Format "yyyy"
 download
